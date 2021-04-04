3ce9035f2777f241b7441fd0cc5b65d91dfd888d
ab9777 <57923364+ab9777@users.noreply.github.com>
2021-04-04 16:31:57 +0530

    added gsap animation

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ hideMenu.addEventListener('click', () => {
 
 // copy code
 
-// const tl = gsap.timeline( { defaults : { ease : "power2.inOut",smoothChildTiming: true}});
+ const tl = gsap.timeline( { defaults : { ease : "power2.inOut",smoothChildTiming: true}});
 
-// tl.to("span", { y: "0%", duration: 1,immediateRender: false, stagger: 0.35});
-// tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5});
-// tl.to(".intro", { y: "-100%", duration: 1}, "-=1.2");
+tl.to("span", { y: "0%", duration: 1,immediateRender: false, stagger: 0.35});
+ tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5});
+ tl.to(".intro", { y: "-100%", duration: 1}, "-=1.2");
 
-// tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: .5}, "-=.4");
-// tl.fromTo("#heading", { opacity: 0 }, { opacity: 1, duration: 1}, "-=1");
-// tl.fromTo("main", { opacity: 0 }, { opacity: 1, duration: 1}, "-=.5");
+ tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: .5}, "-=.4");
+ tl.fromTo("#heading", { opacity: 0 }, { opacity: 1, duration: 1}, "-=1");
+ tl.fromTo("main", { opacity: 0 }, { opacity: 1, duration: 1}, "-=.5");
 
